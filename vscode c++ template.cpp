196a174e96d249f8a368541d21b3e03741f80ccb
DasAnkush <dasankush9284@gmail.com>
2022-01-13 12:12:17 +0530

    initial commit

---
+++ vscode c++ template.cpp
@@ -0,0 +1,149 @@
+//header files
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <set> 
+#include <unordered_set>
+#include <map> 
+#include <unordered_map>
+#include <priority_queue>
+#include <deque>
+#include <algorithm>
+#include <string>
+
+//input output
+cout<<  <<endl;
+
+cin>>  ;
+
+for(int i=0; i<n ; i++){
+  cin>>arr[i];
+}
+
+for(int i=0; i<n ; i++){
+  cout<<arr[i]<<" ";
+}
+
+for(int i=0; i<n ; i++){
+  cin>>arr[i];
+}
+for(int i=0; i<n ; i++){
+  cout<<arr[i]<<" ";
+}
+
+
+char ch[n];
+for(int i=0; i<n ; i++){
+  cin>>ch[i];
+}
+for(int i=0; i<n ; i++){
+  cout<<ch[i]<<" ";
+}
+
+
+int arr[n];
+for(int i=0; i<n ; i++){
+  cin>>arr[i];
+}
+for(int i=0; i<n ; i++){
+  cout<<arr[i]<<" ";
+}
+
+//aggregate functions
+max(  ,  )
+
+sqrt(  )
+
+log(  )
+
+log10(  )
+
+log2(  )
+
+sin(  )
+
+cos(  )
+
+tan(  )
+
+asin(  )
+
+acos(  )
+
+atan(  )
+
+//string
+toupper()
+
+//loops
+for(int i=0; i<n ; i++){
+  
+}
+
+for(int i=n; i>0 ; i--){
+  
+}
+
+while(){
+  
+} 
+
+//conditional statements
+if(  ){
+  ;
+}
+
+else{
+  ;
+}
+
+if(  ){
+  ;
+}
+else{
+  ;
+}
+ if(  ){
+  ;
+}
+else if(  ){
+  ;
+}
+else{
+  ;
+}
+else if(  ){
+    
+}
+
+//templated statements
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve();
+
+int main(){
+    
+    solve();
+    return 0;
+}
+
+void solve() {
+
+}
+//for cp
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,n;
+    cin>>a>>b;
+    
+    return 0;
+}
+
+//standard template
